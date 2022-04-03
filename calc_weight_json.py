72dea26aef5321d167fdb7116b96c22b197f3aa6
danson <dansonkalaghe@gmail.com>
2022-04-03 20:31:03 +0000

    file handlers

---
+++ calc_weight_json.py
@@ -0,0 +1,14 @@
+from jsonFileHandler import *
+
+data = jsonFileHandler.readJsonFile('files/insulin.json')
+
+if data != "" :
+    bInsulin = data['molecules']['bInsulin']
+    aInsulin = data['molecules']['aInsulin']
+    insulin = bInsulin + aInsulin
+    molecularWeightInsulinActual = data['molecularWeightInsulinActual']
+    print('bInsulin: ' + bInsulin)
+    print('aInsulin: ' + aInsulin)
+    print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
+else:
+    print("Error. Exiting program")
