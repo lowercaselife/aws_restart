2172b7af0e1bd9adb6039ec1004168dfaf4ff6fa
danson <dansonkalaghe@gmail.com>
2022-04-03 21:18:14 +0000

    debug

--- calc_weight_json.py
+++ calc_weight_json.py
@@ -10,5 +10,16 @@ if data != "" :
     print('bInsulin: ' + bInsulin)
     print('aInsulin: ' + aInsulin)
     print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
+    # Calculating the molecular weight of insulin  
+    # Getting a list of the amino acid (AA) weights  
+    aaWeights = data['weights']
+    # Count the number of each amino acids  
+    aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A','C','D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T','V', 'W', 'Y']})  
+    # Multiply the count by the weights  
+    molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+    ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T', 'V', 'W', 'Y']}.values())  
+    print("The rough molecular weight of insulin: " +
+    str(molecularWeightInsulin))
+    print("Percent error: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
 else:
     print("Error. Exiting program")
